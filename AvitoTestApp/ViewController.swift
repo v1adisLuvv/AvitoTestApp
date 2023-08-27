1b5719d02300438e6efeb081a026b517467ea5eb
Vlad Boguzh <boguzhvlad@gmail.com>
2023-08-27 21:00:37 +0300

    Initial Commit

---
+++ AvitoTestApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AvitoTestApp
+//
+//  Created by Vlad Boguzh on 2023-08-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
